cdb47f8a152b8c7da812e6d72b6c97664fd3a815
elethuelza03 <elethuelza03@gmail.com>
2025-11-12 09:54:16 +0000

    Changes before Firebase Studio auto-run

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 p-4 text-center">
-      <p>&copy; 2024 Trading Dashboard. All rights reserved.</p>
-    </footer>
-  );
-}
